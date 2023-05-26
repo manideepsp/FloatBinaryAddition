a2092923b853754ff274b68aa076860eea73278d
Manideep <manideepsp.16@gmail.com>
2023-05-26 10:28:00 +0530

    Add project files.

---
+++ FloatBinaryAddition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace FloatBinaryAddition
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string m = Console.ReadLine();
+            string[] num1, num2;
+            string n = Console.ReadLine();
+
+            num1 = m.Split('.');
+            num2 = n.Split('.');
+
+
+
+        }
+    }
+}
